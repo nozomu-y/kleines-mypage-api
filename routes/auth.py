a52e071531c493bd66867fd8a6bef9e6cab9decd
nozoyama39 <nozoyama39@gmail.com>
2021-08-19 00:34:17 +0900

    Update: check if user is not resigned & return admin role

--- routes/auth.py
+++ routes/auth.py
@@ -2,11 +2,12 @@ from flask import jsonify, make_response, request, Blueprint
 import jwt
 import datetime
 import functools
-from models import Users
+from models import Users, Admins
 import subprocess
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+from peewee import JOIN
 
 load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
@@ -21,26 +22,40 @@ auth = Blueprint('auth', __name__)
 def index():
     email = request.json['email']
     password = request.json['password']
-    query = Users.select(
+    query_user = Users.select(
         Users.user_id,
         Users.email,
-        Users.password, Users.status
+        Users.password,
+        Users.status
     ).where(Users.email == email)
-    if len(query) == 0:
+    if len(query_user) == 0:
         result = {"Error": "Email Address Not Found"}
         return make_response(jsonify(result), 400)
-    elif len(query) > 1:    # not neccessary, just in case
+    elif len(query_user) > 1:    # not neccessary, just in case
         result = {"Error": "Duplicate Email Address"}
         return make_response(jsonify(result), 400)
-    script = "php -r 'echo password_verify(\"{0}\",\"{1}\") ? \"true\" : \"false\";'".format(password, query[0].password.replace('$', '\$'))
+    if query_user[0].status == 'RESIGNED':
+        result = {"Error": "Login of Resigned User Not Allowed"}
+        return make_response(jsonify(result), 400)
+    script = "php -r 'echo password_verify(\"{0}\",\"{1}\") ? \"true\" : \"false\";'".format(password, query_user[0].password.replace('$', '\$'))
     ret = subprocess.Popen([script], stdout=subprocess.PIPE, shell=True)
-    (out, err) = ret.communicate()
+    (out, _) = ret.communicate()
     if out.decode('utf-8') != "true":
         result = {"Error": "Passsword Incorrect"}
         return make_response(jsonify(result), 400)
+    # check if user is admin
+    query_admin = Admins.select(
+        Admins.user_id,
+        Admins.role
+    ).where(Admins.user_id == query_user[0].user_id)
+    if len(query_admin) == 0:
+        admin_role = 'GENERAL'
+    else:
+        admin_role = query_admin[0].role
     exp = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
-    encoded = jwt.encode({'name': query[0].user_id, 'exp': exp}, SECRET_KEY, algorithm="HS256")
-    result = {'user_id': query[0].user_id, 'token': encoded}
+    jwt_name = '%d_%s' % (query_user[0].user_id, admin_role)
+    encoded = jwt.encode({'name': jwt_name, 'exp': exp}, SECRET_KEY, algorithm="HS256")
+    result = {'user_id': query_user[0].user_id, 'token': encoded, 'role': admin_role}
     return make_response(jsonify(result))
 
 
@@ -59,6 +74,7 @@ def login_required(method):
         try:
             decoded = jwt.decode(token, SECRET_KEY, algorithms='HS256')
             user_id = decoded['name']
+            print(user_id)
         except jwt.DecodeError:
             result = {"Error": "Token Not Valid"}
             return make_response(jsonify(result), 400)
